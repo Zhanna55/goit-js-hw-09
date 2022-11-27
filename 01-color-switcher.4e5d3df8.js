53da2ae16221ffbf7f248397fb27f3873366a0d1
Zhanna55 <108237376+Zhanna55@users.noreply.github.com>
2022-11-27 20:08:23 +0000

    Deploying to gh-pages from @ Zhanna55/goit-js-hw-09@f01b84715eb17715482bed628bbb16883db1529c 🚀

---
+++ 01-color-switcher.4e5d3df8.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]")};let e=null;t.start.addEventListener("click",(function(){e=setInterval((()=>document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3),t.start.disabled=!0})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.4e5d3df8.js.map
